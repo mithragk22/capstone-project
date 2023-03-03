f4d38b577e50f95b6696b2dd883e05d24eba4cfa
mithragk22 <123443028+mithragk22@users.noreply.github.com>
2023-03-03 11:03:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,14 @@
-def myfunc():
-    return 1
-print(myfunc())
-#vars=
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+    errorhere@@@=== #!!
+    return '<h1>Hello World, \
+            My Name is Akila.</h1>
+            '<h3>Welcome to Python World</h3'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
