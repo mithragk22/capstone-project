9148a764ea9a6c13dc962f8e7a20eb7302dd58fd
mithragk22 <123443028+mithragk22@users.noreply.github.com>
2023-03-03 15:41:36 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,10 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-
-    errorhere@@@=== #!!
-    return '<h1>Welcome to the world, \
-            My Name is Akila.</h1>'
+    html = f"<h1><p>Hello World, my name is Akila</h1></p>"
+    return html.format(format)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
